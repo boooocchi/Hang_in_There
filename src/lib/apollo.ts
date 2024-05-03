d01682d137c472dbf686bde77131aefbfc936d89
Kota Ito <boooocchi@gmail.com>
2024-05-03 14:42:39 -0700

    changed the uri in apollo file

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_BASE_URL,
   cache: new InMemoryCache(),
 });
 
