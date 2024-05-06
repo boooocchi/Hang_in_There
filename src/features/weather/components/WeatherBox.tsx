67951350276df23eb1d197c91c2a6c8135a91fa0
Kota Ito <boooocchi@gmail.com>
2024-05-05 22:55:46 -0700

    adjusted dashboard card design

--- src/features/weather/components/WeatherBox.tsx
+++ src/features/weather/components/WeatherBox.tsx
@@ -5,10 +5,10 @@ import { WeatherData } from '../types/weatherType';
 
 const WeatherBox = ({ weatherData }: WeatherData) => {
   return (
-    <div className="w-full h-full shadow-[5px_10px_10px_-5px_rgba(0,0,0,0.3)] rounded-lg flex items-center justify-center bg-gray py-md max-xs:pl-md max-xs:pr-sm">
+    <div className="w-full h-full shadow-[5px_10px_10px_-5px_rgba(0,0,0,0.3)] rounded-lg flex items-center justify-center bg-gray py-md max-xs:px-sm">
       {weatherData ? (
         <div className="flex flex-col gap-sm xs:flex-row  items-center justify-center xs:items-start w-full">
-          <div className="xs:w-1/3 w-full flex xs:flex-col gap-xs xs:gap-xs justify-center items-center text-xl ">
+          <div className="xs:w-1/3 w-full flex xs:flex-col gap-sm xs:gap-xs justify-center items-center text-xl ">
             <div className="xs:h-[50px] flex items-center justify-center  max-xs:w-1/2">
               <WeatherIcon description={weatherData.weatherDescription} />
             </div>
@@ -17,7 +17,7 @@ const WeatherBox = ({ weatherData }: WeatherData) => {
             </span>
           </div>
           <div className="xs:w-1/3 w-full flex xs:flex-col h-full gap-xs items-center justify-center">
-            <div className="xs:text-2xl text-xl font-boldest ml-2 xs:h-[50px] max-xs:w-1/2 flex items-center justify-center leading-[1.3]">
+            <div className="xs:text-2xl text-xl ml-1 font-boldest xs:h-[50px] max-xs:w-1/2 flex items-center justify-center leading-[1.3]">
               {weatherData.currentTemp}
               <span className="xs:text-base text-sm">°C</span>
             </div>
